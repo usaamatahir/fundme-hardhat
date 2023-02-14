14a21665cad27807d9c6cff0dd996b0a6a407a5c
Usama Tahir <usamatahir8330@gmail.com>
2023-02-14 12:16:02 +0500

    Optimize Code

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,32 +1,37 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.5;
 import "./PriceConverter.sol";
+
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     modifier onlyOwner() {
-        require(
-            msg.sender == owner,
-            "Only owner is allowed to call this function"
-        );
+        // require(
+        //     msg.sender == i_owner,
+        //     "Only owner is allowed to call this function"
+        // );
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
         _;
     }
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
     address[] public funders;
     mapping(address => uint256) public amountFundedByFunder;
 
     // function for sending funds
     function fund() public payable {
         require(
-            msg.value.getConversionRate() > minimumUsd,
+            msg.value.getConversionRate() > MINIMUM_USD,
             "Didn't send enough!"
         );
         funders.push(msg.sender);
@@ -42,5 +47,13 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(success, "Withdraw failed");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
